ecdbcff6dfafa270c4982857d8fb9035ade1b815
Meijirice <Mathieuphan@outlook.com>
2024-09-22 22:10:10 -0400

    hello

--- BackEnd/server.py
+++ BackEnd/server.py
@@ -1,8 +1,17 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.secret_key = "hello"
 cors = CORS(app, origins ="*")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+
+db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
+
 
 
 @app.route('/')
