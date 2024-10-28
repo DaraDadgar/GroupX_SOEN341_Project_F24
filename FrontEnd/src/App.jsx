ca4d641902c5b2ce8fa1bfa310e8ffebce866baa
DESKTOP-KFBNETS <ddadgar1381@gmail.com>
2024-10-28 00:48:42 -0400

    Corrected some of the linting problems with the code previously pushed

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 import "./App.css";
-import axios from "axios";
+
 import "./css/general.css";
 
 import GeneralHomePage from "./routes/GeneralHomePage.jsx";
@@ -11,7 +11,6 @@ import TeamCreation from "./routes/TeamCreation.jsx";
 import Team from "./routes/Team.jsx";
 import MainTeacher from "./routes/MainTeacher.jsx";
 
-import { fetchAPI, storeAPI } from "./functions/apiinterface.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainSignup from "./routes/MainSignup.jsx";
 import Header from "./components/Header.jsx";
@@ -30,7 +29,7 @@ function App() {
         { name: "Name X", id: "123", email: "abc@gmail.com" },
         { name: "Name Y", id: "456", email: "def@gmail.com" },
         { name: "Name Z", id: "789", email: "ghi@gmail.com" },
-        {name: "Name K", id: "555", email: "brand@gmail.com"},
+        { name: "Name K", id: "555", email: "brand@gmail.com" },
       ]); // delete this
     };
 
@@ -45,14 +44,13 @@ function App() {
   }, []);
 
   return (
-    
     <Router>
-      <Header/>
-      <NavBar/>
+      <Header />
+      <NavBar />
       <Routes>
         <Route path="/" element={<GeneralHomePage />} />
         <Route path="/login" element={<MainLogin />} />
-        <Route path="/about" element={<About/>} />
+        <Route path="/about" element={<About />} />
         <Route
           path="/TeamCreation"
           element={<TeamCreation students={TeamCreationStudentsList} />}
@@ -61,7 +59,7 @@ function App() {
         <Route path="/Signup" element={<MainSignup />} />
         <Route path="/Teacher" element={<MainTeacher />} />
         <Route path="/Instructor" element={<Instructor />} />
-        <Route path="/teammodification" element={<TeamModification/>}/>
+        <Route path="/teammodification" element={<TeamModification />} />
       </Routes>
     </Router>
   );
