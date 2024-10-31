f392164cb20c8ec5a883d7aa4ffbc0def51acb0c
Marc-Hab <marchelinohabchi@gmail.com>
2024-10-31 14:11:54 -0400

    Update GeneralHomePage.jsx

--- FrontEnd/src/routes/GeneralHomePage.jsx
+++ FrontEnd/src/routes/GeneralHomePage.jsx
@@ -1,26 +1,23 @@
-import '../css/GeneralHomePage.css'
-import studentImage from "./StudentGroupPicture.jpeg"
-function GeneralHomePage(){
-    return(
-        <>
-            <h1 className="welcome">Welcome to our Peer Assessment Tool!</h1>
+import "../css/GeneralHomePage.css";
+import studentImage from "./StudentGroupPicture.jpeg";
+function GeneralHomePage() {
+  return (
+    <main class="home-page">
+      <h1>Welcome to our Peer Assessment Tool!</h1>
 
-            <div className="description">
-            <img className="student-image" src={studentImage} alt="Student" />
-                <div className='text-image'>
-                    <h2>What we do</h2>
-                    <p>
-                        Our platform is designed to enhance collaborative learning by enabling students to<br />
-                        anonymously evaluate their peers' contributions.  <br />
-                        Instructors benefit from robust management tools<br />
-                        that allow them to efficiently oversee team dynamics and monitor individual and group performance.<br />
-                        With our platform, educators can easily identify strengths and areas for improvement, facilitating targeted support<br />
-<br />                  and enhancing the overall learning experience.<br />
-                    </p>
-                </div>
-            </div>
-        </>
-    );
+      <img className="student-image" src={studentImage} alt="Student" />
+      <h2>What we do</h2>
+      <p>
+        Our platform is designed to enhance collaborative learning by enabling
+        students to anonymously evaluate their peers' contributions. Instructors
+        benefit from robust management tools that allow them to efficiently
+        oversee team dynamics and monitor individual and group performance. With
+        our platform, educators can easily identify strengths and areas for
+        improvement, facilitating targeted support and enhancing the overall
+        learning experience.
+      </p>
+    </main>
+  );
 }
 
-export default GeneralHomePage
+export default GeneralHomePage;
