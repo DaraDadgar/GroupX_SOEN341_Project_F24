a2760d75c93ac443129885226e48e7404040836d
Ouchy123 <orenelohe@gmail.com>
2024-11-02 20:02:36 -0400

    Added code to the HeaderLogout file

---
+++ FrontEnd/src/components/HeaderLogout.jsx
@@ -0,0 +1,23 @@
+import "./css/header-lougout.css";
+import { fetchAPI } from "./functions/apiinterface";
+import { useNavigate } from "react-router-dom";
+
+export default function HeaderLogout() {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try{
+      await fetchAPI("/logout");
+      navigate("/MainLogin");
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
+
+  return (
+    <header>
+      <h1>Peer Assessment Tool</h1>
+      <button className="logout" onClick={handleLogout}>LOGOUT</button>
+    </header>
+  );
+}
