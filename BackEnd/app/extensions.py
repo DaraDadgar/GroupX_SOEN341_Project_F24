3d3dae8ff6780cc846005cc47078e76ea8a3d989
Marc-Hab <marchelinohabchi@gmail.com>
2024-11-04 20:27:09 -0500

    Revert "Logging In using tokens"
    This reverts commit c78edd6c79fd3eaae305703832929198a6ef53d1.

--- BackEnd/app/extensions.py
+++ BackEnd/app/extensions.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
+migrate = Migrate()
 cors = CORS()
-jwt = JWTManager()
--- BackEnd/app/routes/auth_routes.py
+++ BackEnd/app/routes/auth_routes.py
@@ -1,65 +1,50 @@
-from flask import Blueprint, jsonify, request
-from flask_jwt_extended import create_access_token, jwt_required
-
-from app.models import Student, Teacher
+from flask import Blueprint, request, jsonify, session
+from app.models import StudentTeam, Students, Teachers, Teams, Assessments
 from app.extensions import db
 
-# Create a Blueprint for the routes
 auth_bp = Blueprint('auth_bp', __name__)
 
 @auth_bp.route('/login', methods=['POST'])
 def login():
-    try:
-        data = request.json
-        user_info = Student.query.filter_by(email=data['email']).first() if data['user_type']=="student" else Teacher.query.filter_by(email=data['email']).first()
+    email = request.json['email']
+    password = request.json['password']
+    user_type = request.json['type']
 
-        if user_info is None:
-            return jsonify('The email does not exist. Please sign up.')
-        
-        if data['password'] != user_info.password:
-            return jsonify('Wrong password. Please try again')
-        
-        if (data.get('user_type')==""):
-            return jsonify('Invalid Request. Data type not specified.')
+    table = Students if user_type == "student" else Teachers
+    user = table.query.filter_by(email=email, password=password).first()
 
-        access_token = create_access_token(identity=data['email'])
-        return jsonify({'access_token': access_token,'user_type': user_info.user_type() ,'user_info': user_info.to_dict()}), 200
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
+    if user is None:
+        return jsonify({"Response": "ERROR", "type": "None", "Message": "User does not exist"}), 401
+    else:
+        session['email'] = email
+        session['id'] = user.id
+        return jsonify({"Response": "VALID", "type": user_type}), 202
 
 @auth_bp.route('/signup', methods=['POST'])
 def signup():
-    try:
-        data = request.json
-        new_user = Student(first_name=data["first_name"],last_name=data["last_name"],username=data["username"],email=data["email"],
-        password=data["password"]) if data['user_type']=="student" else Teacher(first_name=data["first_name"],last_name=data["last_name"],
-        username=data["username"],email=data["email"],password=data["password"])
+    email = request.json['email']
+    password = request.json['password']
+    user_type = request.json['type']
+    name = request.json['name']
+
+    if user_type == "student":
+        new_user = Students(email=email, password=password, name=name, is_available=True)
+    else:
+        new_user = Teachers(email=email, password=password, name=name)
+
+    table = Students if user_type == "student" else Teachers
+    if table.query.filter_by(email=email).first() is None:
         db.session.add(new_user)
         db.session.commit()
-        student = Student.query.get(1)  # Query the student by ID
+        return jsonify({"Response": "VALID", "type": user_type}), 201
+    else:
+        return jsonify({"Response": "ERROR", "type": "None", "Message": "Email already in use"}), 400
 
-        if student is None:
-            return jsonify({'error': 'Student not added!'}), 404  # Return 404 if not found
+@auth_bp.route('/logout', methods=['GET'])
+def logout():
+    session.clear()
+    return jsonify({"Response": "LOGGED OUT"}), 200
 
-        return jsonify({'id': student.id, 'name': student.first_name}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-    
-@auth_bp.route('/teachers', methods=['GET'])
-def get_teachers():
-    try:
-        teachers = Teacher.query.all()  # Query all students
-        return jsonify([teacher.to_dict() for teacher in teachers]), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-@auth_bp.route('/test', methods=['GET'])
-def test_connection():
-    return jsonify("Request received successfully");
-
-@auth_bp.route('/protected', methods=['GET'])
-@jwt_required()
-def protected():
-    return jsonify("You broke the CORS ass")
+@auth_bp.route('/', methods=['GET'])
+def test():
+    return "test"
