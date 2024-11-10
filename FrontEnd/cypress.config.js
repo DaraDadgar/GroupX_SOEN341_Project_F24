c21e2567c1e4f404b7f0991ed75ca6bca4bc9331
DESKTOP-KFBNETS <ddadgar1381@gmail.com>
2024-11-09 22:38:23 -0500

    converted cjs styling of the config file to js styling

--- FrontEnd/cypress.config.js
+++ FrontEnd/cypress.config.js
@@ -1,6 +1,6 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = import("cypress");
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here if needed
