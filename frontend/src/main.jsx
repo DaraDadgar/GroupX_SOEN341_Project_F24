c863ae2c4f037e36814ce5af5f50899ea52174b6
DESKTOP-KFBNETS <ddadgar1381@gmail.com>
2024-11-28 03:40:30 -0500

    ci testing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 //adding this comment to test the CI workflow
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
